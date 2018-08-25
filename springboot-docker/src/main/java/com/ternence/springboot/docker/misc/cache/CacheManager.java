ba58a33ce6b0e3948d6d385807e8cedfd675226c
陶江航 <ternence.tao@foxmail.com>
2018-08-25 18:13:46 +0800

    增加缓存接口

---
+++ springboot-docker/src/main/java/com/ternence/springboot/docker/misc/cache/CacheManager.java
@@ -0,0 +1,14 @@
+package com.ternence.springboot.docker.misc.cache;
+
+import org.springframework.cache.Cache;
+
+/**
+ * 抽象的缓存管理器
+ *
+ * @author Ternence
+ * @version 1.0
+ * @since 2018/8/25 16:27
+ */
+public interface CacheManager extends Cache{
+
+}
