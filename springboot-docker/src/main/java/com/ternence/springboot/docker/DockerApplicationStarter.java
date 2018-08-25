81afc3a50692e7df20700a179037749734157d0a
陶江航 <ternence.tao@foxmail.com>
2018-08-25 23:10:23 +0800

    优化代码

--- springboot-docker/src/main/java/com/ternence/springboot/docker/DockerApplicationStarter.java
+++ springboot-docker/src/main/java/com/ternence/springboot/docker/DockerApplicationStarter.java
@@ -1,21 +1,19 @@
 package com.ternence.springboot.docker;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
-/**
- * MapperScan注解一定要把 "*" 加上
- */
-@MapperScan(basePackages = {"com.ternence.springboot.docker.mapper.*"},
+@MapperScan(basePackages = {"com.ternence.springboot.docker.mapper"},
         annotationClass = Mapper.class)
 @EnableTransactionManagement
 @SpringBootApplication
 public class DockerApplicationStarter {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DockerApplicationStarter.class);
     }
 }
--- springboot-docker/src/main/java/com/ternence/springboot/docker/mapper/ContactsMapper.java
+++ springboot-docker/src/main/java/com/ternence/springboot/docker/mapper/ContactsMapper.java
@@ -2,10 +2,9 @@ package com.ternence.springboot.docker.mapper;
 
 import com.ternence.springboot.docker.entity.Contacts;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-@Component//将其标记为一个组件，方便idea的智能检测和提示
-@org.apache.ibatis.annotations.Mapper//将其标记为一个Mapper,方便被扫描
+@Component
+@org.apache.ibatis.annotations.Mapper
 public interface ContactsMapper extends Mapper<Contacts> {
 }
